3f10c816ffb0654858b3a335e159786438f2fbe9
Danilo Vieira <danilofritas54@gmail.com>
2022-01-27 17:49:34 -0300

    Primeiro deploy

---
+++ anuncio/models.py
@@ -0,0 +1,21 @@
+from distutils.command.upload import upload
+from email.policy import default
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.models import User
+# Create your models here.
+class Anuncio(models.Model):
+    titulo = models.CharField(max_length=150)
+    foto = models.ImageField(upload_to = 'images/')
+    localizacao = models.CharField(max_length=200)
+    preco = models.FloatField()
+    locador = models.CharField(max_length=50)
+    data_post = models.DateField(auto_now=True)
+    alugado = models.BooleanField(default=False)
+    telefone = PhoneNumberField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+    
+
---
+++ anuncio/urls.py
@@ -0,0 +1,20 @@
+from operator import indexOf
+from re import template
+from django.urls import path
+#from .views import index, meus_anuncios, criar_conta, login_usr, logout_usr
+from django.contrib.auth.views import  LoginView, LogoutView
+from .views import  login_usr, ver_imovel, busca, AnuncioCreate
+from .views import AnuncioUpdate, AnuncioDelete, AnuncioList, MeusAnunciosList, ContaCreate
+urlpatterns = [
+    path('', AnuncioList.as_view(), name = "index"),
+    path('<int:id>/',ver_imovel, name = 'imovel' ),
+    path('buscar/',busca, name = 'busca'),
+    path('cadastrar_anuncio', AnuncioCreate.as_view(), name = 'cadastrar_anuncio'),
+    path('conta/criar_conta', ContaCreate.as_view(), name="criar_conta"),
+    path('conta/login', LoginView.as_view(), name = "login"),
+    path('meus_anuncios/', MeusAnunciosList.as_view(), name = "meus_anuncios"),
+    path('meus_anuncios/<int:pk>', AnuncioUpdate.as_view(), name = "editar_anuncio"),
+    path('meus_anuncios/excluir/<int:pk>', AnuncioDelete.as_view(), name = "excluir_anuncio"),
+    path('conta/logout', LogoutView.as_view(), name='logout')
+
+]
