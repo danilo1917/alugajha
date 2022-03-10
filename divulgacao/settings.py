be30b85cacd4cf48b5fad8357d2096485d9605f2
Danilo Vieira <danilofritas54@gmail.com>
2022-03-10 20:59:18 -0300

    Consertando erros

--- divulgacao/settings.py
+++ divulgacao/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ["https://alugajha.herokuapp.com/","127.0.0.1:8000/" ]
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
